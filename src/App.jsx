dd08e4c3c46356de56d71a74c5682967818a04ee
Makinullizami <agimbal65@gmail.com>
2024-05-03 14:13:22 +0700

    pertama

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Aku</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
