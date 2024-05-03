0026f8159db749970ded4db9cc219d80b461dbc5
Makinullizami <agimbal65@gmail.com>
2024-05-03 15:02:37 +0700

    menambahkan navigation

---
+++ src/screens/Makanan.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const makanan = () => {
+  return (
+    <View>
+      <Text>makanan</Text>
+    </View>
+  );
+};
+
+export default makanan;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/Minuman.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const minuman = () => {
+  return (
+    <View>
+      <Text>minuman</Text>
+    </View>
+  );
+};
+
+export default minuman;
+
+const styles = StyleSheet.create({});
